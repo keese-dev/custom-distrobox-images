b22f3897b2aadd6f9c7f800bcf7ed1bb17d89007
Caden Keese <github@keese.dev>
2025-11-07 20:22:12 +0100

    Update fedora-cosmic.sh to install additional packages
    Enabled extra repositories and installed GitHub CLI.

--- scripts/fedora-cosmic.sh
+++ scripts/fedora-cosmic.sh
@@ -5,4 +5,14 @@
 
 # Update the container and install packages
 dnf update -y
+
+# extra repos
+dnf copr enable atim/starship
+
+sudo dnf install dnf5-plugins
+sudo dnf config-manager addrepo --from-repofile=https://cli.github.com/packages/rpm/gh-cli.repo
+sudo dnf install gh --repo gh-cli
+
+# Update the container and install packages again
+dnf update -y
 grep -v '^#' ./fedora-cosmic.packages | xargs dnf install -y
